edcf6169ddb74cb41c595ce566686ed03eeac756
ping-996 <ping_o96@qq.com>
2021-04-01 18:30:30 +0800

    master

--- src/components/js/coordinate.js
+++ src/components/js/coordinate.js
@@ -17,106 +17,69 @@ export default class Coordinate {
 
     }
     Coordinate() {
+
         var sample_data = [
             {
-                "aiScore": 0.5021412166132017,
-                "aRatio": 0.3879336349924586,
-                "iRatio": 0.7795081967213115,
-                "num_of_elements": 1
+                "displacement (cc)": 360,
+                "power (hp)": 175,
+                "weight (lb)": 3821,
+                "0-60 mph (s)": 11,
             },
-            // {
-            //     "aiScore": 0.5053774019666223,
-            //     "aRatio": 0.9803921568627451,
-            //     "iRatio": 0.9595286885245902,
-            //     "num_of_elements": 1
-            // },
-            // {
-            //     "aiScore": 0.5056444635557789,
-            //     "aRatio": 0.9803921568627451,
-            //     "iRatio": 0.9585040983606558,
-            //     "num_of_elements": 2
-            // }
+            {
+
+                "displacement (cc)": 390,
+                "power (hp)": 190,
+                "weight (lb)": 3850,
+                "0-60 mph (s)": 8.5,
+            }
         ]
+
+        let keys = Object.keys(sample_data[0])
+        let y = new Map(Array.from(keys, key => [key, d3.scaleLinear(d3.extent(sample_data, d => d[key]), [0, 450])]))
+        let x = d3.scalePoint(keys, [0, 550])
+
+        let line = d3.line()
+            .defined(([, value]) => value != null)
+            .x(([key]) => x(key))
+            .y(([key, value]) => y.get(key)(value))
+
         this.SVG = this.map.append("svg")
             .attr("viewBox", `${-this.defaultWH.width / 6} ${-this.defaultWH.height / 7} ${this.defaultWH.width * 1.8} ${this.defaultWH.height * 1.8}`)
             .style("width", "100%")
             .style("height", "auto")
         
-        const width = 550;
-        const height = 400;
-
-        const x = d3.scalePoint()
-            .range([0, width]);
-
-        const y = {};
-        const line = d3.line();
-        const axis = d3.axisLeft();
-        let background;
-        let foreground;
-
-        const dimensions_preset = {
-            support: [0, 1],
-            aiScore: [0.2, 0.9],
-            aRatio: [0, 1],
-            iRatio: [0, 1],
-            num_of_elements: [1, 7],
-        };
-
-        
-
-        let dimensions = Object.keys(sample_data[0])
-        x.domain(dimensions)
-        dimensions.forEach(d => {
-            y[d] = d3.scaleLinear()
-                .domain(dimensions_preset[d] || d3.extent(sample_data, element => +element[d]))
-                .range([height, 0]);
-        });
-
-        const colorDomain = [0.3, 0.6]; // y[colorDimention].domain() // we should make this the same with network view
-        const colorDimention = "aiScore";
-        const lineColor = d => d3.scaleSequential(colorDomain, d3.interpolateRdYlGn)(d[colorDimention]);
-
-        // background = this.SVG.append("g")
-        //     .attr("class", "background")
-        //     .selectAll("path")
-        //     .data(sample_data)
-        //     .join("path")
-        //     .attr("d", path);
-
-        foreground = this.SVG.append("g")
-            .attr("class", "foreground")
+        this.SVG.append("g")
+            .attr("fill", "none")
+            .attr("stroke-width", 3)
+            .attr("stroke-opacity", 1)
             .selectAll("path")
-            .data(sample_data)
+            .data(sample_data)  //sample_data.slice().sort((a, b) => d3.ascending(a[keyz], b[keyz]))
             .join("path")
-            .attr("d", path)
-            .attr('fill', "#98abc5")
-            .style("stroke", "#98abc5")
-            .style("stroke-width", 1);
+            .attr("stroke", '#7b6888')
+            .attr("d", d => line(d3.cross(keys, [d], (key, d) => [key, d[key]])))
+            .append("title")
+            .text(d => d.name);
 
-        // Add a group element for each dimension.
-        const g = this.SVG.selectAll(".dimension")
-            .data(dimensions)
+        this.SVG.append("g")
+            .selectAll("g")
+            .data(keys)
             .join("g")
-            .attr("class", "dimension")
-            .attr("transform", function (d) { 
-                return "translate(" + x(d) + ")"; });
-        
-        // Add an axis and title.
-        g.append("g")
-            .attr("class", "axis")
-            .each(function (d) { d3.select(this).call(axis.scale(y[d])); })
-            .append("text")
-            .style("text-anchor", "middle")
-            .attr("y", -9)
-            .text(function (d) { return d; });
+            .attr("transform", d => `translate(${x(d)},0)`)
+            .each(function (d) { d3.select(this).call(d3.axisLeft(y.get(d))); })
+            .call(g => g.append("text")
+                .attr("x", -30)
+                .attr("y", 470)
+                .attr("text-anchor", "start")
+                .attr("fill", "currentColor")
+                .text(d => d))
+            .call(g => g.selectAll("text")
+                .clone(true).lower()
+                .attr("fill", "none")
+                .attr("stroke-width", 5)
+                .attr("stroke-linejoin", "round")
+                .attr("stroke", "white"));
 
-        function path(d) {
-           
-            let point = dimensions.map(function (p) {
-                console.log(p)
-                return [x(p), y[p](d[p])];
-            })
-            return line(point);
-        }
+
+
     }
 }
