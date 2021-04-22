87fe951e99ebadd828639cc607fa472f86e3bd10
ping_996 <ping_996@qq.com>
2021-04-23 01:18:25 +0800

    master

--- src/views/draw/result.vue
+++ src/views/draw/result.vue
@@ -12,6 +12,7 @@ import axios from "axios";
 import * as d3 from "d3";
 import forceView from "../draw/forceView.vue";
 import store from "@/store";
+
 export default {
   name: "result",
   components: {
@@ -38,7 +39,6 @@ export default {
           // 已经存在VennResult散点图  则删除
           if (box.length == 1) box[box.length - 1].remove();
           this.VennResult(this.$refs.venn, res.data.Vennresult);
-
         });
     },
   },
@@ -66,7 +66,7 @@ export default {
         .append("svg")
         .attr(
           "viewBox",
-          `${-config.width / 17} ${-config.height /5} ${config.width * 1.9} ${
+          `${-config.width / 17} ${-config.height / 5} ${config.width * 1.9} ${
             config.height * 1.9
           }`
         )
@@ -235,35 +235,37 @@ export default {
       for (var i = 0; i < keys.length; i++) {
         let area = data[keys[i]];
         for (var j = 0; j < area.length; j++) {
-          var asi = [0,0];
-          switch (i) {  //["He", "KG", "IN", "HeKG", "HeNI", "KGNI", "HeKGNI"]
+          var asi = [0, 0];
+          switch (
+            i //["He", "KG", "IN", "HeKG", "HeNI", "KGNI", "HeKGNI"]
+          ) {
             case 0:
               asi = recInHetGNN();
-              asi.push('H');
+              asi.push("H");
               break;
             case 1:
               asi = recInKGAT();
-              asi.push('K')
+              asi.push("K");
               break;
             case 2:
               asi = recInNIRec();
-              asi.push('IN');
+              asi.push("IN");
               break;
             case 3:
               asi = recInHK();
-              asi.push('HK');
+              asi.push("HK");
               break;
             case 4:
               asi = recInHN();
-              asi.push('HeNI');
+              asi.push("HeNI");
               break;
             case 5:
               asi = recInKN();
-              asi.push('KGNI');
+              asi.push("KGNI");
               break;
             case 6:
               asi = recInHKN();
-              asi.push('HeKGNI');
+              asi.push("HeKGNI");
               break;
             default:
               break;
@@ -353,7 +355,7 @@ export default {
         let y = _y; // random(_y - 80, _y);
         return [x, y];
       }
-      console.log(data)
+      console.log(data);
       // HetGNN
       drwacircle
         .append("g")
@@ -368,7 +370,7 @@ export default {
         .attr("cy", (d) => d.y)
         .on("click", (event, d) => {
           //小矩形的id  例，u217
-          store.commit("global/SET_MOVIE_ID", d.movieId+'_'+d.or);
+          store.commit("global/SET_MOVIE_ID", d.movieId + "_" + d.or);
         })
         .attr("transform", `rotate(${60}, ${0} ${0})`)
         .attr("r", 6)
@@ -390,7 +392,7 @@ export default {
         .attr("r", 6)
         .on("click", (event, d) => {
           //小矩形的id  例，u217
-          store.commit("global/SET_MOVIE_ID", d.movieId+'_'+d.or);
+          store.commit("global/SET_MOVIE_ID", d.movieId + "_" + d.or);
         })
         .append("title")
         .text((d) => d.movieName);
@@ -409,7 +411,7 @@ export default {
         .attr("r", 6)
         .on("click", (event, d) => {
           //小矩形的id  例，u217
-          store.commit("global/SET_MOVIE_ID", d.movieId+'_'+d.or);
+          store.commit("global/SET_MOVIE_ID", d.movieId + "_" + d.or);
         })
         .attr("transform", `rotate(${-60}, ${0} ${0})`)
         .append("title")
@@ -429,7 +431,7 @@ export default {
         .attr("r", 6)
         .on("click", (event, d) => {
           //小矩形的id  例，u217
-          store.commit("global/SET_MOVIE_ID", d.movieId+'_'+d.or);
+          store.commit("global/SET_MOVIE_ID", d.movieId + "_" + d.or);
         })
         .append("title")
         .text((d) => d.movieName);
@@ -448,11 +450,11 @@ export default {
         .attr("r", 6)
         .on("click", (event, d) => {
           //小矩形的id  例，u217
-          store.commit("global/SET_MOVIE_ID", d.movieId+'_'+d.or);
+          store.commit("global/SET_MOVIE_ID", d.movieId + "_" + d.or);
         })
         .append("title")
         .text((d) => d.movieName);
-      
+
       drwacircle
         .append("g")
         .attr("fill", "#A77669")
@@ -467,11 +469,11 @@ export default {
         .attr("r", 6)
         .on("click", (event, d) => {
           //小矩形的id  例，u217
-          store.commit("global/SET_MOVIE_ID", d.movieId+'_'+d.or);
+          store.commit("global/SET_MOVIE_ID", d.movieId + "_" + d.or);
         })
         .append("title")
         .text((d) => d.movieName);
-      
+
       drwacircle
         .append("g")
         .attr("fill", "#786A88")
@@ -486,62 +488,344 @@ export default {
         .attr("r", 6)
         .on("click", (event, d) => {
           //小矩形的id  例，u217
-          store.commit("global/SET_MOVIE_ID", d.movieId+'_'+d.or);
+          store.commit("global/SET_MOVIE_ID", d.movieId + "_" + d.or);
         })
         .append("title")
         .text((d) => d.movieName);
     },
-    word(){
-      let testdata = {
-        
-      }
+    word() {
+      let data = {
+        values: [],
+        names: [
+          "剧情",
+          "犯罪",
+          "情色",
+          "喜剧",
+          "爱情",
+          "动作",
+          "西部",
+          "冒险",
+          "音乐",
+          "传记",
+          "悬疑",
+          "惊悚",
+          "恐怖",
+          "纪录片",
+          "历史",
+          "战争",
+          "奇幻",
+          "运动",
+          "同性",
+          "动画",
+          "短片",
+          "儿童",
+          "歌舞",
+          "科幻",
+          "家庭",
+          "武侠",
+          "古装",
+          "灾难",
+          "真人秀",
+          "黑色电影",
+          "脱口秀",
+          "戏曲",
+          "舞台艺术",
+          "鬼怪",
+          "荒诞",
+        ],
+        years: [
+          1880,
+          1890,
+          1900,
+          1910,
+          1920,
+          1930,
+          1940,
+          1950,
+          1960,
+          1970,
+          1980,
+          1990,
+          2000,
+          2010,
+          2020,
+        ],
+      };
       const config = {
-        width: parseInt(d3.select('#Word').style("width")),
-        height: parseInt(d3.select('#Word').style("height")),
+        width: parseInt(d3.select("#Word").style("width")),
+        height: parseInt(d3.select("#Word").style("height")),
       };
 
+      const margin = {
+        top: 20,
+        right: 1,
+        bottom: 40,
+        left: 10,
+      };
+
+      const height = 10;
+      const width = 300;
+      const innerHeight = height * data.years.length;
+      const color = d3.scaleSequentialSqrt([0, 20], d3.interpolateInferno); //d3.max(data.values, d => d3.max(d))
+      const x = d3
+        .scaleLinear()
+        .domain([d3.min(data.years), d3.max(data.years) + 1])
+        .rangeRound([margin.left, width - margin.right]);
+      const y = d3
+        .scaleBand()
+        .domain(data.names)
+        .rangeRound([margin.top, margin.top + data.names.length * height]);
+
       const SVG = d3
-        .select('#Word')
+        .select("#Word")
         .append("svg")
-        .attr(
-          "viewBox",
-          `${-config.width / 17} ${-config.height /5} ${config.width * 1.9} ${
-            config.height * 1.9
-          }`
-        )
+        .attr("viewBox", [
+          0,
+          0,
+          width,
+          innerHeight + margin.top + margin.bottom + 70,
+        ])
         .style("width", "100%")
         .style("height", "auto")
-      
+        .style("font", "10px sans-serif");
 
-    }
+      SVG.append("g")
+        .attr("transform", "translate(10,250)")
+        .append(() =>
+          legend({ color, title: "Word Frequency per year", width: 260 })
+        );
+
+      SVG.append("g")
+        .attr("transform", `translate(0,${margin.top})`)
+        .call(d3.axisTop(y).ticks(null, "d"))
+        .call((g) => g.select(".domain").remove());
+
+      SVG.append("g")
+        .attr("transform", `translate(${margin.left+10},0)`)
+        .call(d3.axisLeft(x).tickSize(0))
+        .call((g) => g.select(".domain").remove());
+
+      function legend({
+        color,
+        title,
+        tickSize = 6,
+        width = 320,
+        height = 44 + tickSize,
+        marginTop = 18,
+        marginRight = 0,
+        marginBottom = 16 + tickSize,
+        marginLeft = 0,
+        ticks = width / 64,
+        tickFormat,
+        tickValues,
+      } = {}) {
+        const svg = d3
+          .create("svg")
+          .attr("width", width)
+          .attr("height", height)
+          .attr("viewBox", [0, 0, width, height])
+          .style("overflow", "visible")
+          .style("display", "block");
+
+        let tickAdjust = (g) =>
+          g
+            .selectAll(".tick line")
+            .attr("y1", marginTop + marginBottom - height);
+        let x;
+
+        // Continuous
+        if (color.interpolate) {
+          const n = Math.min(color.domain().length, color.range().length);
+
+          x = color
+            .copy()
+            .rangeRound(
+              d3.quantize(d3.interpolate(marginLeft, width - marginRight), n)
+            );
+
+          svg
+            .append("image")
+            .attr("x", marginLeft)
+            .attr("y", marginTop)
+            .attr("width", width - marginLeft - marginRight)
+            .attr("height", height - marginTop - marginBottom)
+            .attr("preserveAspectRatio", "none")
+            .attr(
+              "xlink:href",
+              ramp(
+                color.copy().domain(d3.quantize(d3.interpolate(0, 1), n))
+              ).toDataURL()
+            );
+        }
+
+        // Sequential
+        else if (color.interpolator) {
+          x = Object.assign(
+            color
+              .copy()
+              .interpolator(
+                d3.interpolateRound(marginLeft, width - marginRight)
+              ),
+            {
+              range() {
+                return [marginLeft, width - marginRight];
+              },
+            }
+          );
+
+          svg
+            .append("image")
+            .attr("x", marginLeft)
+            .attr("y", marginTop)
+            .attr("width", width - marginLeft - marginRight)
+            .attr("height", height - marginTop - marginBottom)
+            .attr("preserveAspectRatio", "none")
+            .attr("xlink:href", ramp(color.interpolator()).toDataURL());
+
+          // scaleSequentialQuantile doesn’t implement ticks or tickFormat.
+          if (!x.ticks) {
+            if (tickValues === undefined) {
+              const n = Math.round(ticks + 1);
+              tickValues = d3
+                .range(n)
+                .map((i) => d3.quantile(color.domain(), i / (n - 1)));
+            }
+            if (typeof tickFormat !== "function") {
+              tickFormat = d3.format(
+                tickFormat === undefined ? ",f" : tickFormat
+              );
+            }
+          }
+        }
+
+        // Threshold
+        else if (color.invertExtent) {
+          const thresholds = color.thresholds
+            ? color.thresholds() // scaleQuantize
+            : color.quantiles
+            ? color.quantiles() // scaleQuantile
+            : color.domain(); // scaleThreshold
+
+          const thresholdFormat =
+            tickFormat === undefined
+              ? (d) => d
+              : typeof tickFormat === "string"
+              ? d3.format(tickFormat)
+              : tickFormat;
+
+          x = d3
+            .scaleLinear()
+            .domain([-1, color.range().length - 1])
+            .rangeRound([marginLeft, width - marginRight]);
+
+          svg
+            .append("g")
+            .selectAll("rect")
+            .data(color.range())
+            .join("rect")
+            .attr("x", (d, i) => x(i - 1))
+            .attr("y", marginTop)
+            .attr("width", (d, i) => x(i) - x(i - 1))
+            .attr("height", height - marginTop - marginBottom)
+            .attr("fill", (d) => d);
+
+          tickValues = d3.range(thresholds.length);
+          tickFormat = (i) => thresholdFormat(thresholds[i], i);
+        }
+
+        // Ordinal
+        else {
+          x = d3
+            .scaleBand()
+            .domain(color.domain())
+            .rangeRound([marginLeft, width - marginRight]);
+
+          svg
+            .append("g")
+            .selectAll("rect")
+            .data(color.domain())
+            .join("rect")
+            .attr("x", x)
+            .attr("y", marginTop)
+            .attr("width", Math.max(0, x.bandwidth() - 1))
+            .attr("height", height - marginTop - marginBottom)
+            .attr("fill", color);
+
+          tickAdjust = () => {};
+        }
+
+        svg
+          .append("g")
+          .attr("transform", `translate(0,${height - marginBottom})`)
+          .call(
+            d3
+              .axisBottom(x)
+              .ticks(
+                ticks,
+                typeof tickFormat === "string" ? tickFormat : undefined
+              )
+              .tickFormat(
+                typeof tickFormat === "function" ? tickFormat : undefined
+              )
+              .tickSize(tickSize)
+              .tickValues(tickValues)
+          )
+          .call(tickAdjust)
+          .call((g) => g.select(".domain").remove())
+          .call((g) =>
+            g
+              .append("text")
+              .attr("x", marginLeft)
+              .attr("y", marginTop + marginBottom - height - 6)
+              .attr("fill", "currentColor")
+              .attr("text-anchor", "start")
+              .attr("font-weight", "bold")
+              .attr("class", "title")
+              .text(title)
+          );
+
+        return svg.node();
+      }
+
+      function ramp(color, n = 256) {
+        var canvas = document.createElement("canvas");
+        canvas.width = n;
+        canvas.height = 1;
+        const context = canvas.getContext("2d");
+        for (let i = 0; i < n; ++i) {
+          context.fillStyle = color(i / (n - 1));
+          context.fillRect(i, 0, 1, 1);
+        }
+        return canvas;
+      }
+    },
   },
 };
 </script>
 <style lang="stylus" scoped>
 #result {
-  width: 70%;
+  width: 100rt%;
   height: 100%;
   border: 1px solid #A6A6A6;
 }
 
 #coordinate {
-  width: 35%;
+  width: 33%;
   height: 30%;
   float: left;
   border-right: 1px solid #A6A6A6;
 }
 
 #venn {
-  width: 32%;
+  width: 30%;
   height: 30%;
   float: left;
   border-right: 1px solid #A6A6A6;
 }
 
 #Word {
-  width: 32%;
+  width: 36%;
   height: 30%;
   float: left;
-  border: 1px solid red;
 }
 </style>
