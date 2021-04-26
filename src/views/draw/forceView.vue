b8eca0f3026e0a92816ec7e7836bd3c3ae788d79
ping-996 <ping_o96@qq.com>
2021-04-26 18:21:34 +0800

    master

--- src/views/draw/forceView.vue
+++ src/views/draw/forceView.vue
@@ -13,42 +13,394 @@ export default {
   },
   created: function () {},
   mounted: function () {
-    // this.force();
+    let data = {
+      links: [
+        {
+          source: "u49",
+          target: "m3255",
+          value: 1,
+        },
+        {
+          source: "m3255",
+          target: "ugroup0",
+          value: 1,
+        },
+        {
+          source: "ugroup0",
+          target: "m793",
+          value: 1,
+        },
+        {
+          source: "u49",
+          target: "m4568",
+          value: 1,
+        },
+        {
+          source: "m4568",
+          target: "a46",
+          value: 1,
+        },
+        {
+          source: "a46",
+          target: "m793",
+          value: 1,
+        },
+        {
+          source: "u49",
+          target: "m4721",
+          value: 1,
+        },
+        {
+          source: "m4721",
+          target: "ugroup1",
+          value: 1,
+        },
+        {
+          source: "ugroup1",
+          target: "m793",
+          value: 1,
+        },
+        {
+          source: "u49",
+          target: "m5061",
+          value: 1,
+        },
+        {
+          source: "m5061",
+          target: "u171",
+          value: 1,
+        },
+        {
+          source: "u171",
+          target: "m793",
+          value: 1,
+        },
+        {
+          source: "m5061",
+          target: "ugroup2",
+          value: 1,
+        },
+        {
+          source: "ugroup2",
+          target: "m793",
+          value: 1,
+        },
+        {
+          source: "u49",
+          target: "m6093",
+          value: 1,
+        },
+        {
+          source: "m6093",
+          target: "ugroup3",
+          value: 1,
+        },
+        {
+          source: "ugroup3",
+          target: "m793",
+          value: 1,
+        },
+        {
+          source: "u49",
+          target: "m6094",
+          value: 1,
+        },
+        {
+          source: "m6094",
+          target: "ugroup4",
+          value: 1,
+        },
+        {
+          source: "ugroup4",
+          target: "m793",
+          value: 1,
+        },
+        {
+          source: "u49",
+          target: "m8125",
+          value: 1,
+        },
+        {
+          source: "m8125",
+          target: "u1283",
+          value: 1,
+        },
+        {
+          source: "u1283",
+          target: "m793",
+          value: 1,
+        },
+        {
+          source: "u49",
+          target: "m8320",
+          value: 1,
+        },
+        {
+          source: "m8320",
+          target: "ugroup6",
+          value: 1,
+        },
+        {
+          source: "ugroup6",
+          target: "m793",
+          value: 1,
+        },
+        {
+          source: "u49",
+          target: "m9034",
+          value: 1,
+        },
+        {
+          source: "m9034",
+          target: "ugroup7",
+          value: 1,
+        },
+        {
+          source: "ugroup7",
+          target: "m793",
+          value: 1,
+        },
+        {
+          source: "u49",
+          target: "m9652",
+          value: 1,
+        },
+        {
+          source: "m9652",
+          target: "u171",
+          value: 1,
+        },
+        {
+          source: "m9652",
+          target: "ugroup8",
+          value: 1,
+        },
+        {
+          source: "ugroup8",
+          target: "m793",
+          value: 1,
+        },
+        {
+          source: "u49",
+          target: "m9934",
+          value: 1,
+        },
+        {
+          source: "m9934",
+          target: "ugroup9",
+          value: 1,
+        },
+        {
+          source: "ugroup9",
+          target: "m793",
+          value: 1,
+        },
+        {
+          source: "u49",
+          target: "m11248",
+          value: 1,
+        },
+        {
+          source: "m11248",
+          target: "a46",
+          value: 1,
+        },
+        {
+          source: "u49",
+          target: "m12213",
+          value: 1,
+        },
+        {
+          source: "m12213",
+          target: "a1211",
+          value: 1,
+        },
+        {
+          source: "a1211",
+          target: "m793",
+          value: 1,
+        },
+      ],
+      nodes: [
+        {
+          id: "u49",
+          value: 10,
+          type: "targetUser",
+        },
+        {
+          id: "m793",
+          value: 10,
+          type: "targetMovie",
+        },
+        {
+          id: "m3255",
+          value: 6,
+          type: "movie",
+        },
+        {
+          id: "ugroup0",
+          value: 7,
+          type: "user",
+        },
+        {
+          id: "m4568",
+          value: 6,
+          type: "movie",
+        },
+        {
+          id: "a46",
+          value: 6,
+          type: "actor",
+        },
+        {
+          id: "m4721",
+          value: 6,
+          type: "movie",
+        },
+        {
+          id: "ugroup1",
+          value: 9,
+          type: "user",
+        },
+        {
+          id: "m5061",
+          value: 6,
+          type: "movie",
+        },
+        {
+          id: "u171",
+          value: 6,
+          type: "user",
+        },
+        {
+          id: "ugroup2",
+          value: 9,
+          type: "user",
+        },
+        {
+          id: "m6093",
+          value: 6,
+          type: "movie",
+        },
+        {
+          id: "ugroup3",
+          value: 7,
+          type: "user",
+        },
+        {
+          id: "m6094",
+          value: 6,
+          type: "movie",
+        },
+        {
+          id: "ugroup4",
+          value: 7,
+          type: "user",
+        },
+        {
+          id: "m8125",
+          value: 6,
+          type: "movie",
+        },
+        {
+          id: "u1283",
+          value: 6,
+          type: "user",
+        },
+        {
+          id: "m8320",
+          value: 6,
+          type: "movie",
+        },
+        {
+          id: "ugroup6",
+          value: 6,
+          type: "user",
+        },
+        {
+          id: "m9034",
+          value: 6,
+          type: "movie",
+        },
+        {
+          id: "ugroup7",
+          value: 7,
+          type: "user",
+        },
+        {
+          id: "m9652",
+          value: 6,
+          type: "movie",
+        },
+        {
+          id: "ugroup8",
+          value: 6,
+          type: "user",
+        },
+        {
+          id: "m9934",
+          value: 6,
+          type: "movie",
+        },
+        {
+          id: "ugroup9",
+          value: 7,
+          type: "user",
+        },
+        {
+          id: "m11248",
+          value: 6,
+          type: "movie",
+        },
+        {
+          id: "m12213",
+          value: 6,
+          type: "movie",
+        },
+        {
+          id: "a1211",
+          value: 6,
+          type: "actor",
+        },
+      ],
+    };
+    this.force(this.$refs.force, data);
   },
   watch: {
-    "$store.getters.movieId"() {
-      // console.log(this.$store.getters.movieId);
-      axios
-        .post("api/getdata/selectedMovie", {
-          data: [this.$store.getters.userId, this.$store.getters.movieId],
-        })
-        .then((res) => {
-          console.log(res.data)
-            let svgbox = document.getElementsByClassName("force");
-            if (svgbox.length == 1) svgbox[svgbox.length - 1].remove();
-            this.force(this.$refs.force, res.data)
-        });
-    },
+    // "$store.getters.movieId"() {
+    //   // console.log(this.$store.getters.movieId);
+    //   axios
+    //     .post("api/getdata/selectedMovie", {
+    //       data: [this.$store.getters.userId, this.$store.getters.movieId],
+    //     })
+    //     .then((res) => {
+    //         let svgbox = document.getElementsByClassName("force");
+    //         if (svgbox.length == 1) svgbox[svgbox.length - 1].remove();
+    //         console.log(res.data)
+    //         this.force(this.$refs.force, res.data)
+    //     });
+    // },
   },
   methods: {
-    force(map,data) {
+    force(map, data) {
+      var net,
+        expand = {};
       const config = {
         width: parseInt(d3.select(map).style("width")),
         height: parseInt(d3.select(map).style("height")),
       };
       const links = data.links.map((d) => Object.create(d));
       const nodes = data.nodes.map((d) => Object.create(d));
-      const SVG = d3
-        .select(map)
-        .append("svg")
-        .attr("class", "force")
-        .attr(
-              "viewBox",
-              `${-config.width / 4} ${-config.height / 7} ${config.width} ${
-                config.height
-              }`
-            )
-      const types = ["targetUser","targetMovie","movie","actor","director","genre","user"]
+      
+      const types = [
+        "targetUser",
+        "targetMovie",
+        "movie",
+        "actor",
+        "director",
+        "genre",
+        "user",
+      ];
       const colors = [
         "#797FA1",
         "#708EC4",
@@ -62,76 +414,104 @@ export default {
         "#65D5A0",
         "#C2384F",
       ];
+      function getGroup(n) {
+        return n.ugroup;
+      }
+      function network(data, prev, index, expand) {
+        expand = expand || {};
+      }
+      // 绘图
+      const SVG = d3
+        .select(map)
+        .append("svg")
+        .attr("class", "force")
+        .attr(
+          "viewBox",
+          `${-config.width / 4} ${-config.height / 7} ${config.width} ${
+            config.height
+          }`
+        );
 
-      const simulation = d3
-        .forceSimulation(nodes)
-        .force(
-          "link",
-          d3.forceLink(links).id((d) => d.id)
-        )
-        .force("x", d3.forceX())
-        .force("y", d3.forceY())
-        .force("charge", d3.forceManyBody().strength(-500))
-        .force("center", d3.forceCenter(600 / 2, 600 / 2));
+      init()
 
-    
+      function init() {
+        // net = network(data, net, getGroup, expand);
 
-      const drag = (simulation) => {
-        function dragstarted(event) {
-          if (!event.active) simulation.alphaTarget(0.3).restart();
-          event.subject.fx = event.subject.x;
-          event.subject.fy = event.subject.y;
-        }
+        const simulation = d3
+          .forceSimulation(nodes)
+          .force(
+            "link",
+            d3.forceLink(links).id((d) => d.id)
+          )
+          .force("x", d3.forceX())
+          .force("y", d3.forceY())
+          .force("charge", d3.forceManyBody().strength(-500))
+          .force("center", d3.forceCenter(600 / 2, 600 / 2));
 
-        function dragged(event) {
-          event.subject.fx = event.x;
-          event.subject.fy = event.y;
-        }
+        const drag = (simulation) => {
+          function dragstarted(event) {
+            if (!event.active) simulation.alphaTarget(0.3).restart();
+            event.subject.fx = event.subject.x;
+            event.subject.fy = event.subject.y;
+          }
 
-        function dragended(event) {
-          if (!event.active) simulation.alphaTarget(0);
-          event.subject.fx = null;
-          event.subject.fy = null;
-        }
+          function dragged(event) {
+            event.subject.fx = event.x;
+            event.subject.fy = event.y;
+          }
 
-        return d3
-          .drag()
-          .on("start", dragstarted)
-          .on("drag", dragged)
-          .on("end", dragended);
-      };
+          function dragended(event) {
+            if (!event.active) simulation.alphaTarget(0);
+            event.subject.fx = null;
+            event.subject.fy = null;
+          }
 
-      const link = SVG.append("g")
-        .attr("stroke", "#999")
-        .attr("stroke-opacity", 0.6)
-        .selectAll("line")
-        .data(links)
-        .join("line")
-        .attr("stroke-width", (d) => Math.sqrt(d.value));
+          return d3
+            .drag()
+            .on("start", dragstarted)
+            .on("drag", dragged)
+            .on("end", dragended);
+        };
 
-      const node = SVG.append("g")
-        .attr("stroke", "#fff")
-        .attr("stroke-width", 1.5)
-        .selectAll("circle")
-        .data(nodes)
-        .join("circle")
-        .attr("r", (d) => d.value)
-        .attr('id', (d,i)=> i)
-        .attr("fill", (d) => colors[types.indexOf(d.type)])
-        .call(drag(simulation));
+        const link = SVG.append("g")
+          .attr("stroke", "#999")
+          .attr("stroke-opacity", 0.6)
+          .selectAll("line")
+          .data(links)
+          .join("line")
+          .attr("stroke-width", (d) => Math.sqrt(d.value));
 
-      node.append("title").text((d) => d.id);
+        const node = SVG.append("g")
+          .attr("stroke", "#fff")
+          .attr("stroke-width", 1.5)
+          .selectAll("circle")
+          .data(nodes)
+          .join("circle")
+          .attr("r", (d) => d.value)
+          .attr("id", (d, i) => i)
+          .attr("fill", (d) => colors[types.indexOf(d.type)])
+          .on("click", (event, d) => {
+                if(d.type != 'targetUser' || d.type != 'targetMovie') {
+                  console.log("node clink",d , arguments, this, expand[d.id])
+                  expand[d.id] = !expand[d.id];  //取反
+                  console.log(expand)
+                  // init();
+                }
+              })
+          .call(drag(simulation));
 
-      simulation.on("tick", () => {
-        link
-          .attr("x1", (d) => d.source.x)
-          .attr("y1", (d) => d.source.y)
-          .attr("x2", (d) => d.target.x)
-          .attr("y2", (d) => d.target.y);
+        node.append("title").text((d) => d.id);
 
-        node.attr("cx", (d) => d.x).attr("cy", (d) => d.y);
-      });
+        simulation.on("tick", () => {
+          link
+            .attr("x1", (d) => d.source.x)
+            .attr("y1", (d) => d.source.y)
+            .attr("x2", (d) => d.target.x)
+            .attr("y2", (d) => d.target.y);
 
+          node.attr("cx", (d) => d.x).attr("cy", (d) => d.y);
+        });
+      }
     },
   },
 };
